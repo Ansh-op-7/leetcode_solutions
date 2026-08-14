7cbc40a2a746b80ea3bae879beec862ee55c34d9
Ansh Choudhary <anshchaudhary88688@gmail.com>
2026-08-15 02:24:01 +0530

    Solved 2137 - Final Value of Variable After Performing Operations

---
+++ Array/2137-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        int i = 0;
+        while (i < operations.length) {
+            if (operations[i].equals("++X") || operations[i].equals("X++")) {
+                x++;
+            } else {
+                x--;
+            }
+            i++;
+        }
+        return x;
+    }
+}
