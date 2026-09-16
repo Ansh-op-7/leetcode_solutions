6dec8aeb5bdda588b44ed7b9fea1b0cb315ef8f6
Ansh Choudhary <anshchaudhary88688@gmail.com>
2026-09-16 16:53:43 +0530

    Solved 1725 - Number of Sets of K Non-Overlapping Line Segments

---
+++ Math/1725-number-of-sets-of-k-non-overlapping-line-segments.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numberOfSets(int n, int k) {
+        int mod = 1000000007;
+        int[][] f = new int[n + 1][k + 1];
+        int[][] g = new int[n + 1][k + 1];
+
+        f[1][0] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                f[i][j] = (f[i - 1][j] + g[i - 1][j]) % mod;
+                g[i][j] = g[i - 1][j];
+
+                if (j > 0) {
+                    g[i][j] = (g[i][j] + f[i - 1][j - 1]) % mod;
+                    g[i][j] = (g[i][j] + g[i - 1][j - 1]) % mod;
+                }
+            }
+        }
+
+        return (f[n][k] + g[n][k]) % mod;
+    }
+}
